a5133654cc741181ae04a33590cd51cfe3322200
Ridam-Mittal <ridam112004@gmail.com>
2025-07-02 21:49:22 +0530

    debugging for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MainMenu from './components/MainMenu';
 import Quiz from './components/Quiz';
 import EndScreen from './components/EndScreen';
 import { QuizContext } from './helpers/Context';
-import './app.css'
+import './App.css';
 
 function App() {
   const [gameState, setGameState] = useState("menu");
